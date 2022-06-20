a5c410b26f6daeb673819a505a897812ce1b76d1
lackbfun <git@lackb.fun>
2022-06-20 15:25:15 +0800

    fix: Added a lot of fault-tolerant judgments

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ https://raw.githubusercontent.com/1ackbfun/dragon-cave-helper/main/data/dragon_d
 https://cdn.jsdelivr.net/gh/1ackbfun/dragon-cave-helper@main/data/dragon_db.json
 ```
 
-[JSON Hero](https://jsonhero.io/)
+[JSON Hero](https://jsonhero.io/) [Example](https://jsonhero.io/j/ixrJ50Rm0xbT)
--- web_spider/get_dragon_data.py
+++ web_spider/get_dragon_data.py
@@ -6,6 +6,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def similar(str1: str, str2: str) -> float:
+    str1 = str1 + ' ' * (len(str2) - len(str1))
+    str2 = str2 + ' ' * (len(str1) - len(str2))
+    return sum(1 if i == j else 0
+               for i, j in zip(str1, str2)) / float(len(str1))
+
+
 class Spider():
     'Dragon Cave Wiki 爬虫'
 
@@ -49,10 +56,8 @@ class Spider():
                         if len(data) < 3:
                             continue
                         try:
-                            breed = [
-                                re.sub(r'\[.*\]$', '', data[2].text.strip()),
-                                data[0].find('img')['alt'].split(' egg')[0],
-                            ]
+                            breed = re.sub(r'\[.*\]$', '',
+                                           data[2].text.strip())
                             habitats = []
                             if len(data) > 3:
                                 for habitat_str in data[3].find_all('a'):
@@ -64,11 +69,10 @@ class Spider():
                                         ])
                                     else:
                                         habitats.append(habitat)
-                            result[breed[0]] = {
-                                'breed':
-                                breed,
+                            result[breed] = {
+                                'breed': [breed, ''],
                                 'egg': [
-                                    re.sub(r'\[.*\]$', '',
+                                    re.sub(r'\[.+\]$', '',
                                            data[1].text.strip()), ''
                                 ],
                                 'rarity':
@@ -95,28 +99,39 @@ class Spider():
                 for table in soup.select('.article-table > tbody'):
                     if table.find('th').text.strip() != '蛋':
                         continue
+                    temp_egg_desc = ''
+                    temp_count = 0
                     for raw_data in table.find_all('tr'):
                         data = raw_data.find_all('td')
                         if len(data) < 3:
                             continue
+                        if 'rowspan' in data[1].attrs:
+                            temp_egg_desc = data[1].text
+                            temp_count = int(data[1]['rowspan']) - 1
                         try:
-                            egg_desc = data[1].text
-                            separator_index = egg_desc.rfind('.')
+                            if temp_count > 0 and len(data) == 3:
+                                egg_desc = temp_egg_desc
+                                temp_count -= 1
+                            else:
+                                egg_desc = data[1].text
+                            separator_index = re.search(
+                                '[\\u4e00-\\u9fa5]',
+                                egg_desc).span()[0]  #egg_desc.rfind('.')
                             result.append({
                                 'breed':
                                 data[0].find('img')['alt'].split(' egg')[0],
                                 'breed_chs':
                                 re.sub(r'\[.*\]$', '', data[2].text.strip()),
                                 'egg':
-                                f'{egg_desc[:separator_index]}.',
+                                f'{egg_desc[:separator_index-1]}.',
                                 'egg_chs':
                                 re.sub(r'\[.*\]$', '',
-                                       egg_desc[separator_index + 1:].strip())
+                                       egg_desc[separator_index:].strip())
                             })
                         except Exception as e:
                             print(f'[ERROR] {e}')
                             print(raw_data.prettify())
-            print(f'[INFO] 处理完成 成功解析 {len(result)} 组数据')
+            print(f'[INFO] {method} 处理完成 成功解析 {len(result)} 组数据')
             return result
 
         wiki = {
@@ -139,19 +154,48 @@ class Spider():
         result = parseHTML(
             captureHTML(wiki[target]['url'], wiki[target]['path'], True),
             target)
+        print('[INFO] 开始整合数据...\n')
         for dragon in result:
             dragon_name = dragon['breed']
             if dragon_name in database:
                 database[dragon_name]['breed'][1] = dragon['breed_chs']
                 if database[dragon_name]['egg'][0] == dragon['egg']:
                     database[dragon_name]['egg'][1] = dragon['egg_chs']
+                elif similar(database[dragon_name]['egg'][0],
+                             dragon['egg']) > 0.9:
+                    # TODO 抉择: 使用中文 Wiki 还是英文 Wiki 的版本
+                    database[dragon_name]['egg'][1] = dragon['egg_chs']
                 else:
                     print(
-                        f"[WARN] 两次采集到的龙蛋描述出现差异: {dragon_name} ({dragon['breed_chs']})\n英文 Wiki: {database[dragon_name]['egg'][0]}\n中文 Wiki: {dragon['egg']}\n"
+                        f"英文 Wiki: {database[dragon_name]['egg'][0]}\n中文 Wiki: {dragon['egg']}\n[ERROR] 两次采集到的龙蛋描述出现巨大差异: {dragon_name} ({dragon['breed_chs']})\n"
                     )
             else:
-                print(f"[WARN] 在现存数据中没有找到 {dragon_name} ({dragon['egg']})\n")
-        print(database['Witchlight Dragon'])
+                found = False
+                for record_dragon in database:
+                    if database[record_dragon]['egg'][0] == dragon['egg']:
+                        # or dragon['breed'] in database[record_dragon]['breed'][0]
+                        database[record_dragon]['egg'][1] = dragon['egg_chs']
+                        found = True
+                        break
+                    elif similar(database[record_dragon]['egg'][0],
+                                 dragon['egg']) > 0.9:
+                        database[record_dragon]['egg'][1] = dragon['egg_chs']
+                        found = True
+                        print(
+                            f"英文 Wiki: {database[record_dragon]['egg'][0]} ({database[record_dragon]['breed'][0]})"
+                        )
+                        print(f"中文 Wiki: {dragon['egg']} ({dragon['breed']})")
+                        print('[WARN] 检索到相似描述的龙蛋 已尝试应用\n')
+                        break
+                if found:
+                    #print('[DEBUG] 已通过龙蛋描述检索到对应的龙')
+                    pass
+                else:
+                    print(dragon)
+                    print(
+                        f"[ERROR] 在现存数据中没有找到 {dragon_name} ({dragon['egg']})\n"
+                    )
+        print('[DEBUG]', database['Witchlight Dragon'])
         with open(r'data\\dragon_db.json', 'w', encoding='utf8') as f:
             f.write(
                 json.dumps(database,
